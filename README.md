f850b075ab48d4d0eaaa8c29206213bf2eb055cb
Ilya Kolbin <iskolbin@gmail.com>
2017-01-26 16:17:23 +0300

    redesign and README added

---
+++ README.md
@@ -0,0 +1,75 @@
+event
+=====
+
+Lua event emitter/listener implementation. Implementation is using queue, so
+it's always ok to emit messages during processing of previous message. While
+you can use this library as singleton, another possibility is define separate
+pool (see below).
+
+In the library there is defined type `EventPool` which handles emitting and 
+binding of listeners.
+
+```lua
+local eventpool = EventPool.new()
+--or
+local eventpool2 = EventPool()
+```
+
+As a convinience `event` has default pool to use as static functions.
+
+EventPool.new()
+---------------
+
+Creates new `EventPool`.
+
+EventPool:bind( source, listener )
+----------------------------------
+
+Bind `listener` to messages emitable by `source`. After `source` emits any messages
+pool will lookup callbacks from `listener`. Returns `true` on success and `false` + 
+error on fail. Possible errors are:
+
+* event.ERROR_NIL_LISTENER if `listener` is `nil`
+* event.ERROR_LISTENER_ALREADY_BINDED if `listener` is already binded to `source`
+
+```lua
+local src = {}
+local list = {}
+event.bind( src, list ) -- now list listens to src
+```
+
+EventPool:unbind( source, listener )
+------------------------------------
+
+Unbind `listener` from `source`. Returns `true` on success and `false` + error
+on fail. Possible errors are:
+
+* event.ERROR_NIL_LISTENER if `listener` is `nil`
+* event.ERROR_LISTENER_NOT_BINDED if `listener` is not binded to `source`
+
+EventPool:emit( source, message, ... )
+--------------------------------------
+
+Emits `message` from `source`. All binded listeners with field `message` will
+be notified and according method (`message`) will be called with passed varargs.
+Function can return:
+
+* event.STATUS_PROCESSED if events queue was empty
+* event.STATUS_QUEUED if events queue wasn't empty and message was queued
+
+```lua
+local src = { name = 'src', }
+local list1 = { name = 'list1', onMessage = function( self, source, msg )
+	print( self.name, source.name, msg )
+end }
+event.bind( src, list1 )
+event.emit( src, 'onMessage', 42 ) -- will print list1, src, 42
+```
+
+EventPool:notify( source, message, ... )
+----------------------------------------
+
+Lower level of `emit` (internally `emit` is implemented with `notify` this
+function directly notifies all listeners without queueing. This is a bit faster,
+but in many cases it can lead to very hard to catch bugs. It's better just not
+to use this function.
--- event.lua
+++ event.lua
@@ -1,46 +1,61 @@
-local pairs, unpack = _G.pairs, _G.unpack or table.unpack
+local setmetatable, pairs, next, unpack = _G.setmetatable, _G.pairs, _G.next, _G.unpack or table.unpack
 
-local _listeners = setmetatable( {}, {__mode = 'k'} )
-local _n = setmetatable( {}, {__mode = 'k'} )
-local _queue = {}
-local _m = 0
-local _locked = false
+local event = {
+	STATUS_PROCESSED = 'Processed',
+	STATUS_QUEUED = 'Queued',
+	ERROR_NIL_LISTENER = 'Listener is nil',
+	ERROR_LISTENER_ALREADY_BINDED = 'Listener is already binded',
+	ERROR_LISTENER_NOT_BINDED = 'Listener is not binded',
+}
 
-local event = {} 
+local EventPool = {}
 
-function event.bind( source, listener )
+EventPool.__index = EventPool
+
+function EventPool.new()
+	return setmetatable( {
+		_listeners = setmetatable( {}, {__mode = 'k'} ),
+		_queue = {},
+	}, EventPool )
+end
+
+function EventPool:bind( source, listener )
 	if listener ~= nil then
-		local listeners = _listeners[source]
+		local listeners = self._listeners[source]
 		if not listeners then
-			_listeners[source] = {[listener] = listener}
-			_n[source] = 1
+			self._listeners[source] = {[listener] = listener}
 		else
 			if listeners[listener] == nil then
-				_n[source] = _n[source] + 1
+				listeners[listener] = listener
+			else
+				return false, event.ERROR_LISTENER_ALREADY_BINDED
 			end
-			listeners[listener] = listener
 		end
+	else
+		return false, event.ERROR_NIL_LISTENER
 	end
+	return true
 end
 		
-function event.unbind( source, listener )
+function EventPool.unbind( source, listener )
 	if listener ~= nil then
-		local listeners = _listeners[source] 
+		local listeners = self._listeners[source] 
 		if listeners and listeners[listener] then
-			local n = _n[source]
-			if n > 1 then
-				listeners[listener] = nil
-				_n[source] = n - 1
-			else
-				_listeners[source] = nil
-				_n[source] = nil
+			listeners[listener] = nil
+			if not next( listeners ) then	
+				self._listeners[source] = nil
 			end
+		else
+			return false, event.ERROR_LISTENER_NOT_BINDED
 		end
+	else
+		return false, event.ERROR_NIL_LISTENER
 	end
+	return true
 end
 
-local function async( source, message, ... )
-	local listeners = _listeners[source]
+function EventPool:notify( source, message, ... )
+	local listeners = self._listeners[source]
 	if listeners then
 		for listener, _ in pairs( listeners ) do
 			if listener[message] then
@@ -48,29 +63,53 @@ local function async( source, message, ... )
 			end
 		end
 	end
+	return event.STATUS_PROCESSED
 end
 
-event.async = async
-
-function event.emit( source, message, ... )
-	if _locked == false then
-		_locked = true
-		async( source, message, ... )
-		local i = 0
-		while i < _m do
-			i = i + 1
-			async( unpack( _queue[i] ))
+function EventPool:emit( source, message, ... )
+	local queue = self._queue
+	if not queue[1] then
+		queue[1] = true
+		self:notify( source, message, ... )
+		local processed = 2
+		while queue[processed] ~= nil do
+			self:notify( unpack( queue[processed] ))
+			processed = processed + 1
 		end
 
-		if _m > 0 then
-			_queue, _m = {}, 0
+		if processed > 2 then
+			self._queue = {}
+		else
+			self._queue[1] = nil
 		end
 
-		_locked = false
+		return event.STATUS_PROCESSED
 	else
-		_m = _m + 1
-		_queue[_m] = {source, message, ...}
+		queue[#queue+1] = {source, message, ...}
+		return event.STATUS_QUEUED
 	end
+end
+
+event.EventPool = setmetatable( EventPool, { __call = function(_,...)
+	return EventPool.new( ... )
+end })
+
+event._defaultpool = EventPool()
+
+function event.bind( ... )
+	return event._defaultpool:bind( ... )
+end
+
+function event.unbind( ... )
+	return event._defaultpool:unbind( ... )
+end
+
+function event.async( ... )
+	return event._defaultpool:async( ... )
+end
+
+function event.emit( ... )
+	return event._defaultpool:emit( ... )
 end
 
 return event
